87b505e076013bb3f08519054e26e1fb07055ecc
bturnip@bturnip.com <bturnip@bturnip.com>
2022-11-17 21:23:51 -0600

    Adding files for Day 2 puzzle

--- tests/test_day2.py
+++ tests/test_day2.py
@@ -2,56 +2,57 @@
 Test Cases for Day2
 """
 from unittest import TestCase
-import unittest.mock as u
-from day2.day2 import *
+
+import sys
+sys.path.append('/home/bturnip/Documents/Code/python/advent_of_code/AdventOfCode2021/day2')
+from day2 import *
+
 
 class testDay2(TestCase):
     """ Test Cases for Day 2 Advent of Code """
     def test_test(self):
         """ Dummy Test to test nosetests"""
         self.assertEqual(1,1)
-
-    # NB: these test cases are testing code that was moved to the
-    # NB: advent_tools module, since they are likely to be used in
-    # NB: following day's puzzles
-    def test_get_input_file_name(self):
-        """ Test automated file name generation"""
-        expected_file_name = ("/home/bturnip/Documents/Code/python/"\
-                              "advent_of_code/AdventOfCode2021/"\
-                              "day2/day2_input.txt")
-
-        # test with no argument
-        print(get_input_file_name())
-        self.assertEqual(expected_file_name, get_input_file_name())
-
-        # test with day as input parameter
-        expected2 = expected_file_name.replace("day2","day3")
-        self.assertEqual(expected2, get_input_file_name(3))
-
-    def test_get_file_linecount(self):
-        """ Get line count of input file"""
-        expected_line_count = 1000
-        self.assertEqual(expected_line_count,get_file_linecount(get_input_file_name()))
-
-    def test_load_input_file_into_list(self):
-        """ Test input file load """
-        foo = load_input_file_into_list(get_input_file_name())
-
-        expected_len = 1000
-        self.assertEqual(expected_len, len(foo))
-
-
-    def test_load_input_file_into_list_bad_load(self):
-        """ Test that a failed file load is flagged """
-        x=get_file_linecount(get_input_file_name())
-        print(x)
         
-        y=load_input_file_into_list(get_input_file_name())
-        print(y)
+    def test_calculate_position_empty_list(self):
+        """ Test calculate_position() with empty list """
+        expected_result = {"horiz":0,"depth":0}
+        foo = calculate_position()
+        self.assertEqual(foo, expected_result)
         
-#        with u.patch('get_file_linecount',100):
-#            get_file_linecount(get_input_file_name())
-#            load_input_file_into_list(get_input_file_name())
+    def test_calculate_position(self):
+        """ Test calculate_position() with known data """
+        foo = ['forward 5\n', 'down 6\n', 'forward 5\n', 'up 2\n']
+        expected_horizontal = 10
+        expected_depth = 4
+ 
+        # test 1: test horizontal position only
+        result = calculate_position(foo)
+        bar = result["horiz"]
+        self.assertEqual(bar, expected_horizontal)
+        
+        #test 2: test depth position only
+        baz = result["depth"]
+        self.assertEqual(baz, expected_depth)
+        
+        #test 3: test dict result correctness
+        expected_result = {"horiz":10,"depth":4}
+        self.assertEqual(result,expected_result)
+
+    def test_calc_postion_with_aim(self):
+        """ Test calc_postion_with_aim() with known data """
+        # Forward adds to *Horizontal* direction
+        # Up or Down subtracts or adds to *Aim*
+        # When *Aim* !=0, multiply *Aim* by *Forward* and add value to *Depth*
+        foo = ['forward 5\n', 'down 5\n', 'forward 8\n', 'up 3\n', 'down 8\n', 'forward 2\n']
+        expected_results={"horiz":15,"depth":60}
+        bar = calc_postion_with_aim(foo)
+        self.assertEqual(bar,expected_results)
+       
+        
 
 
+        
+        
+        
 
