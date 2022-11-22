fb6515aef43118675ac605cc86cf51934f48623e
bturnip@bturnip.com <bturnip@bturnip.com>
2022-11-21 18:36:12 -0600

    adding load_bingo_draws_from_file()

--- day4/day4.py
+++ day4/day4.py
@@ -3,6 +3,9 @@
 # pylint: disable=C0104, C0123, C0413, E0602, W0511
 import sys
 import random
+
+from os.path import exists as file_exists # one liner to check file
+
 sys.path.append('/home/bturnip/Documents/Code/python/advent_of_code/AdventOfCode2021/advent_tools')
 from advent_tools import *
 
@@ -14,7 +17,7 @@ class Day4(object):
 
     def __init__(self, input_file=None):
         self.bingo_draws = []
-        self.valid_bingo_draw_keywords = ["random","input_file"]
+        self.valid_bingo_draw_keywords = ["random"]
         self.input_file = input_file
         self.bingo_cards = np.array([[[]]])
         self.best_card = 0
@@ -27,6 +30,9 @@ class Day4(object):
         elif type(input_draws) == int:
             self.bingo_draws = [input_draws]
 
+        elif file_exists(input_draws):
+            self.load_bingo_draws_from_file(input_draws)
+
         elif type(input_draws) == str:
             if input_draws.lower() not in self.valid_bingo_draw_keywords:
                 err = f"+++ERROR: \"{input_draws}\" not a valid keyword, " \
@@ -35,22 +41,30 @@ class Day4(object):
             if input_draws.lower() == "random":
                 self.bingo_draws = list(range(100))
                 random.shuffle(self.bingo_draws)
-            elif input_draws.lower() == "input_file":
-                #TODO: implement the file extract
-                self.bingo_draws = [1,2,3,25,35,99]
-                # call a function:
-                #  - confirm we have a file that can be opened
-                #  - open a file
-                #  - read one line
-                #  - close the file
-                #  - strip the newline
-                #  - load read line into list
-                #  - set self.bingo_draws to list
-                #  - extend this to the init:
-                #    - if the file is not null
-                #    - if the file is valid
-                #    - do the function steps listed here
+
         else:
-            err = f"+++ERROR: valid inputs are a list of ints, a single "\
-                  f"int value or a keyword in {self.valid_bingo_draw_keywords}"
+            err = "+++ERROR: valid inputs are a valid file path, a " \
+                  "list of ints, a single int value or a keyword in " \
+                  f"{self.valid_bingo_draw_keywords}"
             raise TypeError(err)
+
+    def load_bingo_draws_from_file(self,input_draws):
+        """ loads first line from input_draws file to bingo_draws """
+        if not file_exists(input_draws):
+            err_mssg = f"+++ERROR: input file [{input_draws}] is not "\
+                       f" a valid file."
+            raise TypeError(err_mssg)
+
+        # load Bingo Draw data into list
+        # note that we only read one line
+        with open(input_draws,'r') as file_stream:
+            draw_data = file_stream.readline()
+            file_stream.close()
+            
+        self.bingo_draws = list(map(int,draw_data.rstrip().split(',')))
+
+
+        #  - extend this to the init:
+        #    - if the file is not null
+        #    - if the file is valid
+        #    - do the function steps listed here
--- tests/test_day4.py
+++ tests/test_day4.py
@@ -9,14 +9,20 @@ sys.path.append('/home/bturnip/Documents/Code/python/advent_of_code/AdventOfCode
 
 from day4 import *
 
+
 dummy_input_file = "foo"
-real_input_file = '/home/bturnip/Documents/Code/python/advent_of_code/AdventOfCode2021/day4/day4_raw_input.txt'
-dummy_input_draws = [1,2,3,25,35,99]
+
+day4_path = "/home/bturnip/Documents/Code/python/advent_of_code/AdventOfCode2021/day4"
+
+sample_input_draws = [14,30,18,8,3,52]
+sample_input_file = f"{day4_path}/day4_raw_sample.txt"
+real_input_file = f"{day4_path}/day4_raw_input.txt"
+
 
 
 class TestDay4(TestCase):
     """ Test Cases for Day 4 Advent of Code """
-    global dummy_input_file,real_input_file, dummy_input_draws
+    #global dummy_input_file,real_input_file, dummy_input_draws
 
     def test_test(self):
         """ Dummy Test to test nosetests"""
@@ -26,11 +32,11 @@ class TestDay4(TestCase):
         """ Test Day4 object creation"""
         test01=Day4()
         self.assertIsInstance(test01,object)
-
+        #-------------------------------------
         test02=Day4(dummy_input_file)
         self.assertIsInstance(test02,object)
         self.assertEqual(dummy_input_file,test02.input_file)
-
+        #-------------------------------------
         test03=Day4(real_input_file)
         self.assertIsInstance(test03,object)
         self.assertEqual(real_input_file,test03.input_file)
@@ -38,30 +44,50 @@ class TestDay4(TestCase):
     def test_set_bingo_draws(self):
         """ Test setting the bingo draws """
         single_val = random.randrange(0,100)
-        test04=Day4()
-        test04.set_bingo_draws(single_val)
-        self.assertEqual([single_val],test04.bingo_draws)
-
+        test10=Day4()
+        test10.set_bingo_draws(single_val)
+        self.assertEqual([single_val],test10.bingo_draws)
+        #-------------------------------------
         input_values = ["Random","RANDOM","random"]
         for i in input_values:
-            test05=Day4()
-            self.assertEqual(0,len(test05.bingo_draws))
-            test05.set_bingo_draws(i)
-            self.assertEqual(100,len(test05.bingo_draws))
-            self.assertNotEqual(list(range(100)),test05.bingo_draws)
+            test11=Day4()
+            self.assertEqual(0,len(test11.bingo_draws))
+            test11.set_bingo_draws(i)
+            self.assertEqual(100,len(test11.bingo_draws))
+            self.assertNotEqual(list(range(100)),test11.bingo_draws)
+        #-------------------------------------
+        test12=Day4()
+        self.assertEqual([],test12.bingo_draws)
+        test12.set_bingo_draws(sample_input_draws)
+        self.assertEqual(sample_input_draws,test12.bingo_draws)
 
-        test06=Day4()
-        self.assertEqual([],test06.bingo_draws)
-        test06.set_bingo_draws(dummy_input_draws)
-        self.assertEqual(dummy_input_draws,test06.bingo_draws)
-
+        #-------------------------------------
         bad_input_vals = [{1:2,3:4}, 123.45, "foo", "randommmm"]
-
-        for bad_i in bad_input_vals:
-            test07=Day4()
-            self.assertRaises(TypeError
-                              ,test07.set_bingo_draws
-                              ,bad_i)
         
-        # test08 load/re-load from the input_file specified at object
-        # creation time.
+        for bad_i in bad_input_vals:
+            test13=Day4()
+            self.assertRaises(TypeError
+                              ,test13.set_bingo_draws
+                              ,bad_i)
+        #-------------------------------------
+        test14=Day4()
+        test14.set_bingo_draws(sample_input_file)
+        self.assertEqual(sample_input_draws
+                         ,test14.bingo_draws)
+        
+        
+
+    def test_load_bingo_draws_from_file(self):
+        """ Test loading bingo_draws from an input file """
+        test20=Day4(sample_input_file)
+        test20.load_bingo_draws_from_file(sample_input_file)
+        self.assertEqual(sample_input_draws,test20.bingo_draws)
+        #-------------------------------------
+        test21=Day4()
+        self.assertRaises(TypeError
+                          ,test21.load_bingo_draws_from_file
+                          ,dummy_input_file)
+        
+        
+        
+        
