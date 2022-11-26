9d0673358449fb0957ac8efffbaede0470d9f0a0
bturnip@bturnip.com <bturnip@bturnip.com>
2022-11-26 14:34:42 -0600

    tests/test_setup_next_day.py
    day6: adding files

---
+++ tests/test_setup_next_day.py
@@ -0,0 +1,17 @@
+"""
+Test Cases for setup_next_day.py
+"""
+# pylint: disable=C0104, E0602, W0602
+from unittest import TestCase
+import sys
+
+
+sys.path.append('/home/bturnip/Documents/Code/python/advent_of_code/AdventOfCode2021/')
+
+class MyTests(TestCase):
+    """ Test Cases """
+
+    def test_test(self):
+        """ Dummy Test to test nosetests"""
+        self.assertEqual(1,1)
+    
