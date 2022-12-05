e3f25a0bb9b3de606c6656aa724813fb05e8d671
bturnip@bturnip.com <bturnip@bturnip.com>
2022-12-04 20:34:17 -0600

    setup scripts: added archive step

--- config/setup_config.py
+++ config/setup_config.py
@@ -7,5 +7,17 @@ ROOT = f'/home/bturnip/Documents/Code/python/advent_of_code/'
 BASEDIR = f'{ROOT}{AOC_VINTAGE}'
 
 TEMPLATES_DIR = f'{BASEDIR}/config/templates'
-ARCHIVE_DIR = f'{ROOT}.archives'
+ARCHIVE_DIR = f'{ROOT}.archives/'
+
+# ---list key: [<template file>, <destination folder>, <new_file_pattern>]
+TEMPLATE_LIST = [
+    ['puzzle_steps.yml', '{DAY_FOLDER}','steps_{DAY_NUM}.yml']
+    ,['setup.cfg','{DAY_FOLDER}','setup.cfg']
+    ,['config_dayNUM.py','{DAY_FOLDER}','config_{DAY_NUM}.py']
+    ,['dayNUM.py','{DAY_FOLDER}','{DAY_NUM}.py']
+    ,['solve_dayNUM.py','{DAY_FOLDER}','solve_{DAY_NUM}.py']
+    ,['requirements_dayNUM.txt','{BASEDIR}','requirements_{DAY_NUM}.txt']
+    ,['test_dayNUM.py','{DAY_FOLDER}/tests','test_{DAY_NUM}.py']
+    ,['dayNUM.yml','{BASEDIR}/.github/workflows','run_{DAY_NUM}.yml']
+    ]
 
