ef549a61c9627264e672f613589377f42c46076d
bturnip@bturnip.com <bturnip@bturnip.com>
2022-12-08 08:41:40 -0600

    day10: adding tests for sample answers

--- day10/tests/test_day10.py
+++ day10/tests/test_day10.py
@@ -14,7 +14,7 @@ class TestDay10(TestCase):
     def setUpClass(cls):
         """ Load data needed by tests """
         global dummy_input_file, sample_file, full_file
-        global sample_file_line_count
+        global sample_file_line_count, part1_sample_answer, part2_sample_answer
         
         # -- input file setup        
         dummy_input_file = "foo.file"
@@ -23,6 +23,8 @@ class TestDay10(TestCase):
         
         # verified data
         sample_file_line_count = 10
+        part1_sample_answer = 26397
+        part2_sample_answer = 288957
 
     def test_test(self):
         """ Dummy Test to test nosetests"""
@@ -60,3 +62,11 @@ class TestDay10(TestCase):
         test90 = Day10()
         test_result = test90.get_answer_key()
         self.assertIsInstance(test_result,dict)
+
+        test91 = Day10(sample_file)
+        results = test91.solve_part1()
+        self.assertEqual(results,part1_sample_answer)
+        
+        test92 = Day10(sample_file)
+        results = test92.solve_part2()
+        self.assertEqual(results,part2_sample_answer)
