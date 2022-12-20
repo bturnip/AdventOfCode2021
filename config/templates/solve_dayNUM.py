0ea6c44c2cddc7e02f0e8f7f0d5bec81920b7b14
bturnip@bturnip.com <bturnip@bturnip.com>
2022-12-20 07:02:40 -0600

    templates: adding CLI args to solver for file selector (sample vs full)

--- config/templates/solve_dayNUM.py
+++ config/templates/solve_dayNUM.py
@@ -1,13 +1,31 @@
 """ Advent Of Code 2021 Day {NUM} driver """
+import argparse
 from {DAY_NUM} import *
 from config_{DAY_NUM} import *
 
-# file chooser -----------------------------------
+#-- parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--samplefile"\
+                    ,help = "run the solver script with the SAMPLE_FILE"\
+                    , action = "store_true")
+parser.add_argument("-f", "--fullfile"
+                    ,help = "run the solver script with the FULL_FILE"\
+                    ,action="store_true")
 
-# -- uncomment one of these file choices below
-this_file = FULL_FILE
-this_file = SAMPLE_FILE
+args = parser.parse_args()
+
+
+# file chooser -----------------------------------
+if args.samplefile:
+    this_file = SAMPLE_FILE
+elif args.fullfile:
+    this_file = FULL_FILE
+else:
+    print("+++INFO: No file option specified, default is SAMPLE_FILE")
+    this_file = SAMPLE_FILE
+
 print(f"+++INFO: using {this_file}...")
+
 
 # puzzle runner ----------------------------------
 print("+++INFO: starting puzzle...")
